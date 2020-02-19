7a6c433ca10c631a1e025b126005a70469851722
julfikar <julfikar6262@gmail.com>
2020-02-19 23:09:51 +0600

    initial added file

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,42 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <!-- <student v-bind:message="text"> </student> -->
+    <student> 
+      <!-- <p slot="para">I am ok</p>
+      <h1 slot="header">are you fine</h1> -->
+      <div slot="header">
+       <h1>Student Bio</h1>
+      </div>
+      <div slot="info">
+       <input type="text" placeholder="Enter Name">
+       <input type="text" placeholder="Enter Phone Number">
+      </div>
+      <div slot="details">
+       <button class="btn btn-primary" v-on:click="subDetails()">Submit</button>
+      </div>
+      <div slot="footer">
+        <p>This is your data</p>
+      </div>
+    </student>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import student from './components/student'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld
+    student
+  },
+  data (){
+    return {
+      text : "Memory Brings back memory"
+    }
   }
 }
 </script>
---
+++ src/components/student.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <!-- {{message}} -->
+      <!-- <slot name="para"></slot>
+        <slot name="header"></slot> -->
+        <form action="">
+            <div id="form-header">
+                <slot name="header"></slot>
+            </div>
+            <div id="form-body">
+                <slot name="info"></slot>
+            </div>
+             <div id="form-button">
+                <slot name="details"></slot>
+            </div>
+            <div id="form-footer">
+                <slot name="footer"></slot>
+            </div>
+        </form>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'student',
+//   props: ['message']
+}
+</script>
+
+<style>
+
+</style>
